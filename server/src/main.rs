40a1b95b906bd4d7f98d22f90d68f15a1cc7c302
lucasbrt <lucasbritodepaula.t@gmail.com>
2025-11-21 12:11:14 -0300

    update main router

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,26 +1,16 @@
-use axum::{
-    Router,
-    response::{Html, IntoResponse},
-    routing::get,
-};
-use std::path::PathBuf;
-use tokio::{fs::read_to_string, net::TcpListener};
+use axum::Router;
+use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 const PORT: u16 = 1919;
 static WASM_FILES_PATH: &str = "pkg";
-
-async fn index() -> impl IntoResponse {
-    let path = PathBuf::from("public/index.html");
-    Html(read_to_string(path).await.expect("failed to read file"))
-}
+static STATIC_FILES_PATH: &str = "public";
 
 #[tokio::main]
 async fn main() {
-    let static_files = ServeDir::new(WASM_FILES_PATH);
-    let app = Router::new()
-        .route("/", get(index))
-        .fallback_service(static_files);
+    let app = Router::new().fallback_service(
+        ServeDir::new(WASM_FILES_PATH).fallback(ServeDir::new(STATIC_FILES_PATH)),
+    );
 
     let listener = TcpListener::bind(format!("0.0.0.0:{}", PORT))
         .await
