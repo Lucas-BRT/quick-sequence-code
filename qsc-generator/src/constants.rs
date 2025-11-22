18a3873e7cc7a015e9316189814e570d28f2486b
lucasbrt <lucasbritodepaula.t@gmail.com>
2025-11-22 07:58:39 -0300

    fix: allow mobile devices to read using a lower version of qrcode

--- qsc-generator/src/constants.rs
+++ qsc-generator/src/constants.rs
@@ -4,4 +4,4 @@ pub const PERFORMANCE_THRESHOLD: usize = 50;
 pub const DEFAULT_QR_CODE_SIZE: u32 = 200;
 pub const METADATA_OVERHEAD: usize = 12;
 pub const ANIMATION_DELAY_INCREMENT: f64 = 0.1;
-pub const DEFAULT_QR_CODE_VERSION: i16 = 12;
+pub const DEFAULT_QR_CODE_VERSION: i16 = 8;
